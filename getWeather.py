012911e004c3d0976f2acdccbcd8b9c487fb08e6
HamzaDGR <HamzaDGR>
2024-04-18 15:18:36 +0200

    TP3 adaptation

--- getWeather.py
+++ getWeather.py
@@ -1,23 +1,29 @@
 from flask import Flask, request, jsonify
-import os
 import requests
 
 app = Flask(__name__)
 
-#Creation de la route 
+
+api_key = '6e1c179eb50ae7f95cc2d7845ad8b6ec'
+
+
+
 @app.route('/weather', methods=['GET'])
 def get_weather():
-    latitude = request.args.get('lat')
-    longitude = request.args.get('lon')
-    api_key = os.environ.get('API_KEY')
+    latitude = request.args.get('LATTITUDE')  
+    longitude = request.args.get('LONGITUDE')  
 
-    if not (latitude and longitude and api_key):
-        raise ValueError("Veuillez indiquer la latitude, longitude et l'API key")
+    if not (latitude and longitude):
+        return jsonify({"error": "Veuillez fournir la latitude et la longitude"}), 400  
 
     url = f"http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
     response = requests.get(url)
-    data = response.json()
-    return jsonify(data)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data), 200  
+    else:
+        return jsonify({"error": "Impossible de récupérer les données météo"}), 500 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)#
+    app.run(debug=True, host='0.0.0.0', port=80)
